a2faa6d6208a6d2e0fa7ce61440c8fde183561d4
sonusamal654 <motilalsamal79@gmail.com>
2025-07-02 12:44:52 -0700

    add 3 files

--- signup.js
+++ signup.js
@@ -7,10 +7,10 @@ let confirm=document.querySelector('#confirm')
 let submit=document.querySelector('#submit')
 const pattern=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-submit.addEventlistener("click",() =>{
+submit.addEventListener("click",() =>{
     let firstData=first.value
     let lastData=last.value
-    let emailtData=email.value
+    let emailData=email.value
     let phoneData=phone.value
     let passwordData=password.value
     let confirmData=confirm.value
@@ -32,4 +32,42 @@ submit.addEventlistener("click",() =>{
         else{
             alert("invalid email")
         }
+    if(!phoneData){
+        alert("enter phone number")
+    }
+    if(phoneData.length!=10){
+        alert("invalid phone number")
+    }
+    else{
+        alert("success number")
+    }
+    if(!passwordData){
+        alert("enter password")
+    }
+    if(passwordData.match(pattern)){
+         alert("success password")
+    }
+    else{
+        alert("password contain one uppercase,lower case,one letter")
+    }
+    if(!confirmData){
+        alert("enter confirm password")
+    }
+    if(confirmData.match(pattern)){
+         alert("success password")
+    }
+    else{
+        alert(" confirm password contain one uppercase,lower case,one letter")
+    }
+    if(passwordData!=confirmData){
+        alert("password mismatch")
+    }
+    if(passwordData.length!=6 && confirmData.length!=6){
+        alert("invalid password")
+        }
+        else{
+            alert("success the password length")
+        }
+        
+    
 })
